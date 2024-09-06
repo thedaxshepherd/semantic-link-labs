024279689cb69ee587af79a3da5a1b69abb57e73
thedaxshepherd <thedaxshepherd@gmail.com>
2024-09-06 09:48:14 -0500

    Testing wheel file

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(
+    	name='jr_Labs', #needs to build fabric
+    	version='1.0.0',
+    	description='A sample library of utilities to use in Fabric notebooks',
+   	 packages=find_packages()
+)
